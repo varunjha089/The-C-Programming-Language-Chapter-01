dd0eb415f51c61153634dcdda85d5ee6e34dd56f
VARUN KUMAR <varunjha089@gmail.com>
2021-09-15 20:09:13 +0530

    exercies 3 completed.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main() {
     int fahr;
 
+    printf("Fahrenheit to Celsius scale\n");
     for (fahr = 0; fahr <= 300; fahr = fahr + 20){
         printf("%3d \t %6.1f \n", fahr, (5.0 /9.0)*(fahr-32));
     }
