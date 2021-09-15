c2910a7c3122f461680fa00ad9e3a8c4c68c7883
VARUN KUMAR <varunjha089@gmail.com>
2021-09-15 19:29:22 +0530

    using floating-point arithmetic instead of integer.

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;
+    lower = -40;
     upper = 300;
     step = 20;
 
     fahr = lower;
     while (fahr <= upper){
-        celsius = 5 * (fahr -32) / 9;
-        printf("%d \t %d\n", fahr, celsius);
+        celsius = (5.0 * (fahr - 32.0) / 9.0);
+        printf("%3.0f \t %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
     return 0;
