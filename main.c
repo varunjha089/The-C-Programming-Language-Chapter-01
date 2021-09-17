9371e70f657385dfd34781e326d1e80e0d4c885f
VARUN KUMAR <varunjha089@gmail.com>
2021-09-17 11:42:51 +0530

    File copying completed.

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
+// 1.5.1 File Copying
 #include <stdio.h>
 
-#define     LOWER   0
-#define     UPPER   300
-#define     STEP    20
-
 int main() {
-    int fahr;
+    int c;
 
-    printf("Fahrenheit to Celsius scale\n");
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP){
-        printf("%3d \t %6.1f \n", fahr, (5.0 /9.0)*(fahr-32));
+    c = getchar();
+    while (c != EOF){
+        putchar(c);
+        c = getchar();
     }
     return 0;
 }
